32a06d7faefe5f7be7f9b55888ffcdcb06530bd9
Mohammed Musaib Ali <64194019+mdmusaibali@users.noreply.github.com>
2021-06-14 00:00:30 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,6 +1,10 @@
 var express=require('express')
 var app=express();
-app.get("/",function(req,res){
+app.get("/",function(err,res){
 res.send("Helllo");
 }):
 
+
+app.listen(8080,function(){
+console.log("connection made");
+});
